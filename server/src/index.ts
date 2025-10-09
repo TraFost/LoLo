32909983979898d0fe1361445fa38c8ca003e1c7
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-09 23:18:41 +0700

    chore: initialize pnpm workspace with client, server, and shared packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,31 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import type { ApiResponse } from 'shared/dist'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { serve } from "@hono/node-server";
+import type { ApiResponse } from "shared/dist";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-app.get('/hello', async (c) => {
+app.get("/hello", async (c) => {
+	const data: ApiResponse = {
+		message: "Keren Kamu fauzul, bisa fizz buzz",
+		success: true,
+	};
 
-  const data: ApiResponse = {
-    message: "Hello BHVR!",
-    success: true
-  }
+	return c.json(data, { status: 200 });
+});
 
-  return c.json(data, { status: 200 })
-})
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
-export default app
+serve({
+	fetch: app.fetch,
+	port,
+});
+
+export default app;
