a2cb88297367749b2c1d3106f507727e406f2ca7
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-10 11:27:48 +0700

    feat(style): add tailwind & shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,37 @@
-import { useState } from 'react';
-import type { ApiResponse } from 'shared';
-
 import mainLogo from '@assets/icon/lolo-main.webp';
 
-import './App.css';
+import { Button } from '@/components/ui/button';
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3000';
+// import { useState } from 'react';
+// import type { ApiResponse } from 'shared';
+
+// const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3000';
 
 function App() {
-  const [data, setData] = useState<ApiResponse | undefined>();
+  // const [data, setData] = useState<ApiResponse | undefined>();
 
-  async function sendRequest() {
-    try {
-      const req = await fetch(`${SERVER_URL}/hello`);
-      const res: ApiResponse = await req.json();
-      setData(res);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // async function sendRequest() {
+  //   try {
+  //     const req = await fetch(`${SERVER_URL}/hello`);
+  //     const res: ApiResponse = await req.json();
+  //     setData(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return (
-    <>
+    <section className="flex items-center justify-center size-full flex-col">
       <div>
         <a href="https://github.com/TraFost/LoLo" target="_blank">
-          <img src={mainLogo} className="logo" alt="LoLo logo" />
+          <img src={mainLogo} className="bg-white" alt="LoLo logo" />
         </a>
       </div>
+
       <h1>LoLo</h1>
-      <div className="card">
-        <div className="button-container">
-          <button onClick={sendRequest}>Call API</button>
-        </div>
-        {data && (
-          <pre className="response">
-            <code>
-              Message: {data.message} <br />
-              Success: {data.success.toString()}
-            </code>
-          </pre>
-        )}
-      </div>
-    </>
+
+      <Button>test</Button>
+    </section>
   );
 }
 
