9a950afce4b890843376c8e00b0f6d83c44e9cf2
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-14 08:00:03 +0700

    fix(app): correct import statement and update JSX structure in AppProviders

--- client/src/core/app/providers.app.tsx
+++ client/src/core/app/providers.app.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from 'react';
 
-import AppRouter from './router.app';
+import { AppRouter } from './router.app';
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <>
+    <main className="size-full">
       <AppRouter />
 
       {children}
-    </>
+    </main>
   );
 }
