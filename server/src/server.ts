c5b97a90ba821bc7607264d15c776910971b7fda
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-19 05:57:42 +0700

    feat(server): initial setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,49 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { serve } from "@hono/node-server";
-import type { ApiResponse } from "shared/dist";
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { csrf } from 'hono/csrf';
+import { logger } from 'hono/logger';
+import { prettyJSON } from 'hono/pretty-json';
+import { secureHeaders } from 'hono/secure-headers';
+
+import { StatusCodes } from 'shared/src/http-status';
+
+import type { ApiResponse } from 'shared/src/types/response';
+
+import { ENV } from './configs/env.config';
+import { CORS } from './configs/cors.config';
 
 const app = new Hono();
+const api = new Hono().basePath('/api');
 
-app.use(cors());
+app.use(cors(CORS));
+app.use(csrf());
+app.use(logger());
+app.use(prettyJSON());
+app.use(secureHeaders());
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+app.get('/', (c) => {
+  return c.text('Hello from LoLo Server!');
 });
 
-app.get("/hello", async (c) => {
-	const data: ApiResponse = {
-		message: "Keren Kamu fauzul, bisa fizz buzz",
-		success: true,
-	};
-
-	return c.json(data, { status: 200 });
+api.post('analyze', (c) => {
+  return c.text('here`s your analyze!');
 });
 
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+app.get('*', (c) => {
+  const data: ApiResponse = {
+    message: 'Endpoint not available Or you might have a typos',
+    success: false,
+  };
+
+  return c.json(data, { status: StatusCodes.NOT_FOUND });
+});
+
+console.log(`Server is running on port ${ENV.port}`);
 
 serve({
-	fetch: app.fetch,
-	port,
+  fetch: app.fetch,
+  port: ENV.port,
 });
 
 export default app;
