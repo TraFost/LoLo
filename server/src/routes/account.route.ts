3013b29d21da9ad19cccc6129d35b4ed2bc6c63e
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-21 20:30:34 +0700

    feat(account): implement account retrieval service and validation schema

---
+++ server/src/routes/account.route.ts
@@ -0,0 +1,37 @@
+import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+
+import { AccountService } from '../services/account.service';
+
+import { accountSchema } from '../schemas/account.schema';
+
+import { zValidator } from '../middlewares/validator.middleware';
+
+import { successWithData } from '../lib/utils/response.util';
+
+import { StatusCodes } from 'shared/src/http-status';
+import type { Region } from 'shared/src/types/account.type';
+
+const app = new Hono();
+
+app.get('/:gameName/:tagLine', zValidator('param', accountSchema), async (c) => {
+  const platformRegion = c.req.query('region') ?? 'ph2';
+
+  const { gameName, tagLine } = c.req.param();
+
+  const accountService = new AccountService(platformRegion as Region);
+
+  try {
+    const data = await accountService.getAccountByRiotId({ gameName, tagLine });
+    return c.json(successWithData('LoL Account Fetched!', data), StatusCodes.OK);
+  } catch (err: unknown) {
+    console.error(err, 'err');
+
+    throw new HTTPException(StatusCodes.BAD_GATEWAY, {
+      message: 'Upstream Riot API not reachable',
+      cause: err,
+    });
+  }
+});
+
+export default app;
