a3149a92ca522ce32bbbb75fc7a05a579cde493c
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-21 09:04:37 +0700

    feat(constants): add REGION_MAP for mapping regions to their respective categories

---
+++ shared/src/constants/match.constant.ts
@@ -0,0 +1,19 @@
+import type { Region } from '../types/account.type';
+
+export const REGION_MAP: Record<string, Region> = {
+  na1: 'americas',
+  br1: 'americas',
+  la1: 'americas',
+  la2: 'americas',
+  euw1: 'europe',
+  eun1: 'europe',
+  tr1: 'europe',
+  ru: 'europe',
+  kr: 'asia',
+  jp1: 'asia',
+  ph2: 'sea',
+  sg2: 'sea',
+  th2: 'sea',
+  tw2: 'sea',
+  vn2: 'sea',
+};
