f304bf5de333942e0dcc2b1e401ce6fdd7cf03c3
Samsul <samsul.panjul97@gmail.com>
2025-11-03 18:21:26 +0700

    refactor(summarize): improve RenderState handling for missing or loading data

--- client/src/features/summarize/page/summarize.page.tsx
+++ client/src/features/summarize/page/summarize.page.tsx
@@ -9,10 +9,9 @@ import {
   PlayerComparisonCard,
   PlayerOverviewCard,
 } from '../components/image-card.summarize';
-import { LoadingSection } from '@/ui/organisms/loading-section.organism';
 import { useFetchStatistics } from '../hooks/statistics/use-fetch-statistics.hook';
-import { ErrorSection } from '@/ui/organisms/error-section.organism';
 import { fillChampions } from '../utils/champion/fill-champions.util';
+import { RenderState } from '../components/render-state.summarize';
 
 export function SummarizePage() {
   const {
@@ -27,41 +26,41 @@ export function SummarizePage() {
     accountError,
   } = useFetchStatistics();
 
-  if (isMissingParams) return <p>Redirecting...</p>;
-
-  if (isAccountLoading) return <LoadingSection />;
-
-  if (isAccountError) return <ErrorSection error={accountError!.message} />;
-
-  if (isLoading) return <LoadingSection />;
-  if (isError) return <ErrorSection error={error!.message} />;
-
-  const champions = fillChampions(statistics!.champions, 5);
+  const champions = fillChampions(statistics?.champions ?? [], 5);
 
   return (
-    <div className="flex flex-col items-center bg-gray-950 text-white">
-      <RecapIntro
-        gameName={accountData!.gameName}
-        tagName={accountData!.tagLine}
-        championName={champions[0].name}
-        profilePict={accountData!.profilePict}
-      />
-      <Statistics statistics={statistics!.statistics} />
-      <ChampionsSummarize champions={champions} />
-      <GameplayOverview gameplayData={statistics!.gameplay} />
-      <ProPlayer />
-      <ImageCardSection>
-        <MostPlayedChampionsCard
-          playerName={`${accountData!.gameName}#${accountData!.tagLine}`}
-          champions={champions}
-        />
-        <PlayerOverviewCard
-          playerName={`${accountData!.gameName}#${accountData!.tagLine}`}
-          statistics={statistics!.statistics}
-          roleDistribution={statistics!.gameplay.roleDistribution}
-        />
-        <PlayerComparisonCard />
-      </ImageCardSection>
-    </div>
+    <RenderState
+      isRedirecting={isMissingParams}
+      isLoading={isAccountLoading || isLoading}
+      isError={isAccountError || isError}
+      error={accountError?.message || error?.message}
+    >
+      {accountData && statistics ? (
+        <div className="flex flex-col items-center bg-gray-950 text-white">
+          <RecapIntro
+            gameName={accountData.gameName}
+            tagName={accountData.tagLine}
+            championName={champions[0].name}
+            profilePict={accountData.profilePict}
+          />
+          <Statistics statistics={statistics.statistics} />
+          <ChampionsSummarize champions={champions} />
+          <GameplayOverview gameplayData={statistics.gameplay} />
+          <ProPlayer />
+          <ImageCardSection>
+            <MostPlayedChampionsCard
+              playerName={`${accountData!.gameName}#${accountData.tagLine}`}
+              champions={champions}
+            />
+            <PlayerOverviewCard
+              playerName={`${accountData.gameName}#${accountData.tagLine}`}
+              statistics={statistics.statistics}
+              roleDistribution={statistics.gameplay.roleDistribution}
+            />
+            <PlayerComparisonCard />
+          </ImageCardSection>
+        </div>
+      ) : null}
+    </RenderState>
   );
 }
