8e5e6aecfb0b6add33e23526c6756ddcbfb8b4bc
Samsul <samsul.panjul97@gmail.com>
2025-11-04 15:44:17 +0700

    fix(loading): separate account loading and statistics loading for better ui readability and add loading progress bar component

--- client/src/features/summarize/components/render-state.summarize.tsx
+++ client/src/features/summarize/components/render-state.summarize.tsx
@@ -3,19 +3,22 @@ import { LoadingSection } from '@/ui/organisms/loading-section.organism';
 
 export function RenderState({
   isRedirecting,
+  isAccountLoading,
   isLoading,
   isError,
   error,
   children,
 }: {
   isRedirecting?: boolean;
+  isAccountLoading?: boolean;
   isLoading?: boolean;
   isError?: boolean;
   error?: string;
   children: React.ReactNode;
 }) {
-  if (isRedirecting) return <LoadingSection />;
-  if (isLoading) return <LoadingSection />;
+  if (isRedirecting) return <LoadingSection isLoading={isLoading} />;
+  if (isAccountLoading) return <LoadingSection isLoading={isAccountLoading} />;
+  if (isLoading) return <LoadingSection isLoading={isLoading} showProgressBar={true} />;
   if (isError) return <ErrorSection error={error || 'Unknown error'} />;
   return <>{children}</>;
 }
