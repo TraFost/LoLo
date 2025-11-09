180385a25090e03266204e6d806c66ae99511b5e
Samsul <samsul.panjul97@gmail.com>
2025-11-09 15:30:59 +0700

    fix(loading): sync progress bar with axios download events

--- client/src/features/summarize/components/render-state.summarize.tsx
+++ client/src/features/summarize/components/render-state.summarize.tsx
@@ -8,17 +8,20 @@ export function RenderState({
   isError,
   error,
   children,
+  progress,
 }: {
   isRedirecting?: boolean;
   isAccountLoading?: boolean;
   isLoading?: boolean;
   isError?: boolean;
   error?: string;
+  progress?: number;
   children: React.ReactNode;
 }) {
   if (isRedirecting) return <LoadingSection isLoading={isLoading} />;
   if (isAccountLoading) return <LoadingSection isLoading={isAccountLoading} />;
-  if (isLoading) return <LoadingSection isLoading={isLoading} showProgressBar={true} />;
+  if (isLoading)
+    return <LoadingSection isLoading={isLoading} showProgressBar={true} progressValue={progress} />;
   if (isError) return <ErrorSection error={error || 'Unknown error'} />;
   return <>{children}</>;
 }
